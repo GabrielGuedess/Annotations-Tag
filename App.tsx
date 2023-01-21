e8c078974300444aae962ca8a8a56ef4a9f70baa
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-01-21 03:51:29 -0300

    feat: add context menu and animate tags

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'react-native';
+
+import { HoldMenuProvider } from 'react-native-hold-menu';
+import { Feather } from '@expo/vector-icons';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    <HoldMenuProvider theme='dark' iconComponent={Feather}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       <Home />
-    </>
+    </HoldMenuProvider>
   );
 }
